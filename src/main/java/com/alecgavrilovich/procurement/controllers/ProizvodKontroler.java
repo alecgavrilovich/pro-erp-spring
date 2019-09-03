95d82042cf3bd83a5f5f462808074eaf4cf57539
Aleksandar Gavrilovic <alecgavrilovich@gmail.com>
2019-09-03 18:08:08 +0200

    bez-obj-novi-pr
    bez-obj-novi-pr

--- src/main/java/com/alecgavrilovich/procurement/controllers/ProizvodKontroler.java
+++ src/main/java/com/alecgavrilovich/procurement/controllers/ProizvodKontroler.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import com.alecgavrilovich.procurement.dbb.ProizvodDBB;
@@ -31,42 +30,18 @@ public class ProizvodKontroler {
 	}
 	
 	@RequestMapping("/noviProizvod")
-	public String noviProizvod(Model model) {
-		
-		model.addAttribute("prForma", pr);
-		// model.addAttribute("listJM", prDBB.vratiJM());
+	public String noviProizvod() {
 		
 		return "proizvod/novi-proizvod";
 		
 	}
 	
-//	@RequestMapping(value="/sacuvajProizvod", method = RequestMethod.POST)
-//	public String sacuvajProizvod(@RequestParam("id") int id, @RequestParam("opisPr") String opisPr, @RequestParam("JmId") int JmId) {
-//		
-////		if(bindingResult.hasErrors()){
-////            return "proizvod/novi-proizvod";
-////        }
-//		
-//		pr.setId(id);
-//		pr.setOpisPr(opisPr);
-//		pr.setJmId(JmId);
-//		
-//		prDBB.sacuvajProizvod(pr);
-//		
-//		
-//		return "redirect:/proizvod/lista";
-//	}
-	
 	@RequestMapping(value="/sacuvajProizvod", method = RequestMethod.POST)
-	public String sacuvajProizvod(Proizvod pr, BindingResult  bindingResult) {
+	public String sacuvajProizvod(@RequestParam("id") int id, @RequestParam("opisPr") String opisPr, @RequestParam("JmId") int JmId) {
 		
-		if(bindingResult.hasErrors()){
-            return "proizvod/novi-proizvod";
-        }
-		
-		pr.setId(pr.getId());
-		pr.setOpisPr(pr.getOpisPr());
-		pr.setJmId(pr.getJmId());
+		pr.setId(id);
+		pr.setOpisPr(opisPr);
+		pr.setJmId(JmId);
 		
 		prDBB.sacuvajProizvod(pr);
 		
