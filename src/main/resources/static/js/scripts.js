2549e42cc6d6ee4ac3362e58c9b50bb7c3e77cfa
Aleksandar Gavrilovic <alecgavrilovich@gmail.com>
2019-09-10 20:43:56 +0200

    forma-za-stavku
    forma-za-stavku

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,16 +1,22 @@
 $( document ).ready(function() {
 	
-    $(".clickable-row").on("click", function() {
-    	location.replace($(this).attr("data-href"))
+	$("#ocistiFormuZaStavku").on("click", function() {
+		$(".polje-nove-stavke").val("");
     });
-    
-    $(".itemRemoveButton").on("click", function(){
-        if($(this).text()=="Remove") {
-            $(this).text("Undo");
-        } else {
-            $(this).text("Remove");
-        }
-    });
+		
+	
+	
+//    $(".clickable-row").on("click", function() {
+//    	location.replace($(this).attr("data-href"))
+//    });
+//    
+//    $(".itemRemoveButton").on("click", function(){
+//        if($(this).text()=="Remove") {
+//            $(this).text("Undo");
+//        } else {
+//            $(this).text("Remove");
+//        }
+//    });
    
     
 });
